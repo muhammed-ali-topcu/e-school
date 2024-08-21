9422f1b800c4bf1aaacfb2b35db447d60356fe91
Muhammed-Ali-01 <m.aligenel01@gmail.com>
2024-08-21 09:12:12 +0300

    add filament

---
+++ app/Filament/Resources/GradeResource.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\GradeResource\Pages;
+use App\Filament\Resources\GradeResource\RelationManagers;
+use App\Models\Grade;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class GradeResource extends Resource
+{
+
+    protected static ?string $model = Grade::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('name')
+                    ->required(),
+                Forms\Components\TextInput::make('sequence')
+                    ->required()
+                    ->numeric()
+                    ->gte(0),
+                Forms\Components\Toggle::make('is_active')
+                    ->default(true)
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('name')->searchable(),
+                Tables\Columns\TextColumn::make('sequence')->searchable()->sortable(),
+                Tables\Columns\BooleanColumn::make('is_active'),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index'  => Pages\ListGrades::route('/'),
+            'create' => Pages\CreateGrade::route('/create'),
+            'edit'   => Pages\EditGrade::route('/{record}/edit'),
+        ];
+    }
+}
