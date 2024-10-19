f2dd36b4196dba218572ed9901ca7389ee6aab24
Muhammed-Ali-01 <m.aligenel01@gmail.com>
2024-10-19 10:44:38 +0300

    lang switcher visible out

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -26,7 +26,8 @@ class AppServiceProvider extends ServiceProvider
         });
         LanguageSwitch::configureUsing(function (LanguageSwitch $switch) {
             $switch
-                ->locales(['tr','en']);
+                ->locales(['tr','en'])
+                ->visible(outsidePanels: true);
         });
     }
 }
