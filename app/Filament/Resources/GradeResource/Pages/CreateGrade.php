d412518ece018a8825e488de873b92df78606a7c
Muhammed-Ali-01 <m.aligenel01@gmail.com>
2024-11-11 11:56:17 +0300

    add patie-laravel-translatable-plugin

--- app/Filament/Resources/GradeResource/Pages/CreateGrade.php
+++ app/Filament/Resources/GradeResource/Pages/CreateGrade.php
@@ -6,7 +6,17 @@ use App\Filament\Resources\GradeResource;
 use Filament\Actions;
 use Filament\Resources\Pages\CreateRecord;
 
+
 class CreateGrade extends CreateRecord
 {
+
+    use CreateRecord\Concerns\Translatable;
+
     protected static string $resource = GradeResource::class;
+    protected function getHeaderActions(): array
+    {
+        return [
+            Actions\LocaleSwitcher::make(),
+        ];
+    }
 }
--- app/Filament/Resources/GradeResource/Pages/EditGrade.php
+++ app/Filament/Resources/GradeResource/Pages/EditGrade.php
@@ -8,12 +8,15 @@ use Filament\Resources\Pages\EditRecord;
 
 class EditGrade extends EditRecord
 {
+    use EditRecord\Concerns\Translatable;
+
     protected static string $resource = GradeResource::class;
 
     protected function getHeaderActions(): array
     {
         return [
             Actions\DeleteAction::make(),
+            Actions\LocaleSwitcher::make(),
         ];
     }
 }
